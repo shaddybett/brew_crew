3e7dd0858f2d821f75b078d7346c39edb4ae9af1
Shaddy <shadybett540@gmail.com>
2025-01-29 11:50:26 +0300

    setup a folder structure for brews

---
+++ lib/wrapper.dart
@@ -0,0 +1,13 @@
+import 'package:brew_crew/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+      child: Home(),
+    );
+  }
+}
