63363bbea547c536285e29672dcc64ad78a77da8
Shaddy <shadybett540@gmail.com>
2025-02-06 09:51:50 +0300

    set up unonimous signin

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -4,5 +4,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService{
 // signin anon
     final FirebaseAuth _auth = FirebaseAuth.instance;
+
+    Future signInAnon() async {
+        try {
+            AuthResult result = await _auth.signInAnonymously();
+            FirebaseUser  user = result;
+        }catch (e){
+
+        }
+    } 
 // signin with email & pass
 }
